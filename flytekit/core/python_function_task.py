0350c7019b2670270f25c3b09a2133e210d6f496
Muskan Kumari <er.muskan09@gmail.com>
2026-04-27 15:07:22 +0530

    Add audit logging to task execution using logger

--- flytekit/core/python_function_task.py
+++ flytekit/core/python_function_task.py
@@ -205,15 +205,28 @@ class PythonFunctionTask(PythonAutoContainerTask[T]):  # type: ignore
             return f"{self.instantiated_in}.{self._name}"
         return self._name
 
-    def execute(self, **kwargs) -> Any:
-        """
-        This method will be invoked to execute the task. If you do decide to override this method you must also
-        handle dynamic tasks or you will no longer be able to use the task as a dynamic task generator.
-        """
-        if self.execution_mode == self.ExecutionBehavior.DEFAULT:
-            return self._task_function(**kwargs)
-        elif self.execution_mode == self.ExecutionBehavior.DYNAMIC:
-            return self.dynamic_execute(self._task_function, **kwargs)
+    from flytekit.loggers import logger
+import time
+
+def execute(self, **kwargs) -> Any:
+    """
+    This method will be invoked to execute the task. If you do decide to override this method you must also
+    handle dynamic tasks or you will no longer be able to use the task as a dynamic task generator.
+    """
+    if self.execution_mode == self.ExecutionBehavior.DEFAULT:
+        start_time = time.time()
+        logger.info(f"[AUDIT] Task {self.name} started")
+
+        result = self._task_function(**kwargs)
+
+        end_time = time.time()
+        logger.info(f"[AUDIT] Task {self.name} finished")
+        logger.info(f"[AUDIT] Duration: {end_time - start_time} seconds")
+
+        return result
+
+    elif self.execution_mode == self.ExecutionBehavior.DYNAMIC:
+        return self.dynamic_execute(self._task_function, **kwargs)
 
     def _create_and_cache_dynamic_workflow(self):
         if self._wf is None:
